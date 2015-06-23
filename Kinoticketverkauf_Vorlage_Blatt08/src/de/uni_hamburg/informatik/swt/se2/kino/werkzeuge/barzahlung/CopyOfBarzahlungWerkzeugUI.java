46e9729ca93be702f58a326db639512bf31df63b
farzinranjbar <farzin@live.de>
2015-06-23 20:30:22 +0200

    Änderung 23.06

--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/CopyOfBarzahlungWerkzeugUI.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/werkzeuge/barzahlung/CopyOfBarzahlungWerkzeugUI.java
@@ -1,13 +1,11 @@
 package de.uni_hamburg.informatik.swt.se2.kino.werkzeuge.barzahlung;
 
-import java.awt.BorderLayout;
-
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+
 import javax.swing.JButton;
 
-public class BarzahlungWerkzeugUI
+public class CopyOfBarzahlungWerkzeugUI
 {
 
     private JFrame _frame;
@@ -20,7 +18,7 @@ public class BarzahlungWerkzeugUI
      /**
      * Create the application.
      */
-    public BarzahlungWerkzeugUI()
+    public CopyOfBarzahlungWerkzeugUI()
     {
         erstellePanel();
     }
@@ -30,18 +28,35 @@ public class BarzahlungWerkzeugUI
      */
     private void erstellePanel()
     {
-        JDialog meinJDialog = new JDialog();
-        meinJDialog.setTitle("Barzahlungs-Prozess");
-        meinJDialog.setSize(200,200);
-        meinJDialog.setVisible(true);
-        
         _frame = new JFrame();
+        _frame.setBounds(100, 100, 450, 300);
         _frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        _frame.getContentPane().setLayout(new BorderLayout());
+        _frame.getContentPane().setLayout(null);
         
+        _restFeld = new JTextField();
+        _restFeld.setEditable(false);
+        _restFeld.setBounds(44, 123, 346, 44);
+        _frame.getContentPane().add(_restFeld);
+        _restFeld.setColumns(10);
         
-
-
+        _okButton = new JButton("OK");
+        _okButton.setBounds(44, 178, 133, 49);
+        _frame.getContentPane().add(_okButton);
+        
+        _abbrechenButton = new JButton("Abbrechen");
+        _abbrechenButton.setBounds(190, 177, 200, 50);
+        _frame.getContentPane().add(_abbrechenButton);
+        
+        _summeFeld = new JTextField();
+        _summeFeld.setBounds(44, 11, 346, 44);
+        _frame.getContentPane().add(_summeFeld);
+        _summeFeld.setColumns(10);
+        _summeFeld.setEditable(false);
+        
+        _eingabePreisFeld = new JTextField();
+        _eingabePreisFeld.setBounds(44, 68, 346, 44);
+        _frame.getContentPane().add(_eingabePreisFeld);
+        _eingabePreisFeld.setColumns(10);
     }
     
     /**
